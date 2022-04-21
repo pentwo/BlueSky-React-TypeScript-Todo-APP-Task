65e3b3ce94c8b42f7e23cc6a9b4a6daf97cb74bc
Steven Peng <pentwo@gmail.com>
2022-04-21 15:08:18 +0800

    fix: workaround for "findDOMNode is deprecated in StrictMode."

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,31 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+
+import {
+  createMuiTheme,
+  ThemeProvider,
+  unstable_createMuiStrictModeTheme,
+} from "@material-ui/core";
 import * as serviceWorker from "./serviceWorker";
 import { makeServer } from "./Server/server";
 
 const server = makeServer();
-// console.log(server)
+
+// For Warning: findDOMNode is deprecated in StrictMode.
+const createTheme =
+  process.env.NODE_ENV === "production"
+    ? createMuiTheme
+    : unstable_createMuiStrictModeTheme;
+const theme = createTheme({
+  // ...
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
