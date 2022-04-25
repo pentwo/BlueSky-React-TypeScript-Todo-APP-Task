3cfcddd48a113981b331679029004f0ce792a756
Steven Peng <pentwo@gmail.com>
2022-04-25 18:41:04 +0800

    Updates

---
+++ precache-manifest.7881d286e070e029fdae2a6d1b80015b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d696d4094a77624909bc872fb7477205",
+    "url": "/BlueSky-React-TypeScript-Todo-APP-Task/index.html"
+  },
+  {
+    "revision": "3ac506e52aaeb3f2a7eb",
+    "url": "/BlueSky-React-TypeScript-Todo-APP-Task/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "3695a8e2d1e4eb501809",
+    "url": "/BlueSky-React-TypeScript-Todo-APP-Task/static/js/2.e4fd5014.chunk.js"
+  },
+  {
+    "revision": "5c4b00a641e6db9ec791fdc1d7ed67ad",
+    "url": "/BlueSky-React-TypeScript-Todo-APP-Task/static/js/2.e4fd5014.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ac506e52aaeb3f2a7eb",
+    "url": "/BlueSky-React-TypeScript-Todo-APP-Task/static/js/main.abe89dde.chunk.js"
+  },
+  {
+    "revision": "6ff9fc502a7e522a6855",
+    "url": "/BlueSky-React-TypeScript-Todo-APP-Task/static/js/runtime-main.ec88809e.js"
+  }
+]);
